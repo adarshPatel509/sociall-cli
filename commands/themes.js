e9abb254e1760529dd815b05a2f273415e1d9a1e
Utsav Khatu <utsavkhatu416@gmail.com>
2020-12-30 23:47:52 +0530

    Themes command added, implementation is Remaining

---
+++ commands/themes.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import { Text, Box } from "ink"
+import SelectInput from 'ink-select-input';
+import PropTypes from 'prop-types';
+const fs = require("fs")
+const th = require('../themes.json')
+
+const Themes = () => {
+    const [updateField, setField] = useState("");
+    // console.log(th)
+
+    const items = [
+        { label: 'Default', value: 'default' },
+        { label: 'Solarized Dark', value: 'sol_dark' },
+        { label: 'Solarized Light', value: 'sol_light' },
+        { label: 'Papercolor', value: 'papercolor' },
+        { label: 'Molokai', value: 'molokai' },
+    ];
+
+    const handleSelect = (item) => {
+        setField(item.value);
+    };
+
+    if (updateField === '') {
+        return (
+            <>
+                <Box borderStyle="round" paddingLeft={1} width={51} borderColor="#00FFFF">
+                    <Text color="yellow">Select theme for the cli: </Text>
+                </Box>
+                <SelectInput items={items} onSelect={handleSelect} />
+            </>
+        );
+    }
+    else {
+        th.current = updateField
+        fs.writeFile('themes.json',JSON.stringify(th), function writeJSON(err) {
+            if (err) return console.log(err);
+          })
+        // console.log(th)
+        return <Text color='red'>Theme Updated to <Text>{updateField}</Text> !!</Text>;
+
+    }
+
+}
+
+
+export default Themes
