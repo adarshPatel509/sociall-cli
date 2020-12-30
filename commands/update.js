386d7ce331382cd57cd6867b19a3470c65824610
Utsav Khatu <utsavkhatu416@gmail.com>
2020-12-31 00:04:09 +0530

    Update Profile Feature Added except for reddit

--- commands/update.js
+++ commands/update.js
@@ -1,83 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import {Text, Box} from 'ink';
-import SelectInput from 'ink-select-input';
-import { UncontrolledTextInput } from 'ink-text-input';
-import UpdateGithubProfile from '../components/update/UpdateGithubProfile';
-import UpdateInstagramProfile from '../components/update/UpdateInstagramProfile';
-import UpdateTwitterProfile from '../components/update/UpdateTwitterProfile';
-
-
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Text, Box } from "ink";
+import SelectInput from "ink-select-input";
+import { UncontrolledTextInput } from "ink-text-input";
+import UpdateGithubProfile from "../components/update/UpdateGithubProfile";
+import UpdateInstagramProfile from "../components/update/UpdateInstagramProfile";
+import UpdateTwitterProfile from "../components/update/UpdateTwitterProfile";
+// import UpdateRedditProfile from "../components/update/UpdateRedditProfile";
 /// Update Your Profile
-const UpdateProfile = ({platform}) => {
-  const [updateField, setField] = useState('');
-  const [newValue, setValue] = useState('');
+const UpdateProfile = ({ platform }) => {
+	const [updateField, setField] = useState("");
+	const [newValue, setValue] = useState("");
 
-  const items = [
-    {label: 'Full Name', value: 'name'},
-    {label: 'UserName', value: 'username'},
-    {label: 'Bio', value: 'bio'}
-  ];
+	const items = [
+		{ label: "Full Name", value: "name" },
+		{ label: "UserName", value: "username" },
+		{ label: "Bio", value: "bio" },
+		{ label: "Profile Photo", value: "profile_photo" },
+	];
 
-  const handleSelect = (item) => {
-    setField(item.value);
-  };
+	const handleSelect = (item) => {
+		setField(item.value);
+	};
 
-  const handleSubmit = (newValue) => {
-    const updateObj = {[updateField]: newValue};
-    setValue(updateObj);
-  }
+	const handleSubmit = (newValue) => {
+		const updateObj = { [updateField]: newValue };
+		setValue(updateObj);
+	};
 
-  if(updateField === '') {
-    return (
-      <>
-        <Box borderStyle="round" paddingLeft={1} width={41} borderColor="#00FFFF">
-          <Text color="yellow">Select the profile section to Update: </Text>
-        </Box>
-        <SelectInput items={items} onSelect={handleSelect} />
-      </>
-    );
-  } 
-  else if(newValue === '') {
-    return (
-      <Box width="100%">
-        <Box marginRight={1}>
-          <Text>Enter new {updateField}:</Text>
-        </Box>
-        <UncontrolledTextInput onSubmit={handleSubmit} />
-      </Box>
-    );
-  } 
-  else {
-    if(platform.includes('all')) {
-      return (
-        <>
-          <UpdateGithubProfile updateObj={newValue} />
-          <UpdateTwitterProfile updateObj={newValue} />
-          <UpdateInstagramProfile updateObj={newValue} />
-        </>
-      );
-    }
-    if(platform.includes('github')) {
-      return <UpdateGithubProfile updateObj={newValue} />;
-    }
-    else if(platform.includes('twitter')) {
-      return <UpdateTwitterProfile updateObj={newValue} />;
-    }
-    else if(platform.includes('instagram')) {
-      return <UpdateInstagramProfile updateObj={newValue} />
-    }
-    return <Text color='red'>Please enter valid platform name!!</Text>;
-  };
+	if (updateField === "") {
+		return (
+			<>
+				<Box
+					borderStyle="round"
+					paddingLeft={1}
+					width={51}
+					borderColor="#00FFFF"
+				>
+					<Text color="yellow">Select the profile section to Update: </Text>
+				</Box>
+				<SelectInput items={items} onSelect={handleSelect} />
+			</>
+		);
+	} else if (newValue === "") {
+		return (
+			<Box width="100%">
+				<Box marginRight={1}>
+					<Text>Enter new {updateField}:</Text>
+				</Box>
+				<UncontrolledTextInput onSubmit={handleSubmit} />
+			</Box>
+		);
+	} else {
+		if (platform.includes("all")) {
+			return (
+				<>
+					<UpdateGithubProfile updateObj={newValue} />
+					<UpdateTwitterProfile updateObj={newValue} />
+					<UpdateInstagramProfile updateObj={newValue} />
+					{/* <UpdateRedditProfile updateObj={newValue} /> */}
+				</>
+			);
+		}
+		if (platform.includes("github")) {
+			return <UpdateGithubProfile updateObj={newValue} />;
+		} else if (platform.includes("twitter")) {
+			return <UpdateTwitterProfile updateObj={newValue} />;
+		} else if (platform.includes("instagram")) {
+			return <UpdateInstagramProfile updateObj={newValue} />;
+		}
+		// else if (platform.includes('reddit')) {
+		//   return <UpdateRedditProfile updateObj={newValue} />
+		// }
+		return <Text color="red">Please enter valid platform name!!</Text>;
+	}
 };
 
 UpdateProfile.propTypes = {
-  ///List of platforms to Profile Update
-  platform: PropTypes.string.isRequired
+	///List of platforms to Profile Update
+	platform: PropTypes.string.isRequired,
 };
 
 UpdateProfile.shortFlags = {
-  platform: 'pf'
+	platform: "pf",
 };
 
 export default UpdateProfile;
--- components/update/UpdateTwitterProfile.js
+++ components/update/UpdateTwitterProfile.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, Box } from 'ink';
 import Loader from '../../utils/loader.js';
 import { twit } from '../../utils/api-clients';
-
+const fs = require("fs")
 
 /** 
     Update Twitter Profile
@@ -12,23 +12,37 @@ const UpdateTwitterProfile = (props) => {
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    if(props.updateObj.bio) {
+    if (Object.keys(props.updateObj)[0] == "profile_photo") {
+      // console.log(props.updateObj.profile_photo);
+      const b64content = fs.readFileSync(props.updateObj.profile_photo, { encoding: 'base64' });
+      twit.post('account/update_profile_image', { image: b64content })
+        .then(res => {
+          setLoading(false)
+        })
+        .catch(err => {
+          console.log(err)
+          setLoading(false)
+        })
+    }
+    else {
+      if (props.updateObj.bio) {
         props.updateObj['description'] = props.updateObj.bio;
         delete props.updateObj.bio;
+      }
+      twit.post('account/update_profile', props.updateObj)
+        .then(res => {
+          setLoading(false);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-    twit.post('account/update_profile', props.updateObj)
-    .then(res => {
-      setLoading(false);
-    })
-    .catch(err => {
-      console.log(err);
-    });
   }, []);
-   
-  if(isLoading) {
+
+  if (isLoading) {
     return <Loader message=" Updating Twitter Profile..." type="dots" />;
   }
-  return <Text>Twitter Profile Updated!!</Text>;
+  return <Text color="greenBright">Twitter Profile Updated!!</Text>;
 }
 
 export default UpdateTwitterProfile;
